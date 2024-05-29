9478be7e4642c99818c9c008aaafc1a9c08d62da
ambercoltman <ST10438993@vcconnect.edu.za>
2024-05-29 23:37:57 +0200

    Added a loop

--- src/main/java/com/mycompany/addingtasks/AddingTasks.java
+++ src/main/java/com/mycompany/addingtasks/AddingTasks.java
@@ -13,35 +13,37 @@ import javax.swing.JOptionPane;
 public class AddingTasks {
 
     public static void main(String[] args) {
-         Tasks user = new Tasks();
+     String taskNumber = "";
+     String developerDetails = "";
+     String taskName = "";
+     String totalHours = "";
+     String taskDetails = "";
+     Tasks user = new Tasks();
          
-         String[] options1 = {"Option 1). Add tasks", "Option 2). Show report", "Option 3). Quit"};
-         var selection = JOptionPane.showOptionDialog(null, "Welcome to EasyKanban","Option", 0, 3 , null, options1, options1[0]);
          
-         if(selection == 0){
-            JOptionPane.showInputDialog(null,"Please enter the amount of tasks"); 
-            
+            String[] options1 = {"Option 1). Add tasks", "Option 2). Show report", "Option 3). Quit"};
+            var selection = JOptionPane.showOptionDialog(null, "Welcome to EasyKanban","Option", 0, 3 , null, options1, options1[0]);
+
+            while(selection == 0){
+                
+                taskNumber = JOptionPane.showInputDialog(null,"Please enter the amount of tasks"); 
+               
             String[] options2 = {"To Do", "Done", "Doing"};          
             selection = JOptionPane.showOptionDialog(null, "Task Status","Status", 0, 3 , null, options2, options2[0]);
          
-            JOptionPane.showInputDialog(null,"Please enter your first and last name");
-          
-            JOptionPane.showInputDialog(null,"Please enter your task name");
+                developerDetails = JOptionPane.showInputDialog(null,"Please enter your first and last name");
 
-            user.taskDescription =JOptionPane.showInputDialog("Please enter the task description");
- 
-            JOptionPane.showInputDialog(null, "Please enter the hours it took you to complete the task");
-         
-         }
-         if(selection == 1){
-            JOptionPane.showMessageDialog(null,"Coming Soon"); 
-         } 
-         
-         //task id 
-         //user.taskID = JOptionPane.showInputDialog
-         
-         
-                 
-         
-    }
+                taskName = JOptionPane.showInputDialog(null,"Please enter your task name");
+
+                user.taskDescription =JOptionPane.showInputDialog("Please enter the task description");
+
+                totalHours = JOptionPane.showInputDialog(null, "Please enter the hours it took you to complete the task");
+        
+                JOptionPane.showMessageDialog(null, " Developer Details: " + developerDetails + "Task Number:" + taskNumber + "Task Name:" + taskName + "Task Description:" + user.taskDescription +"Task Duration:" + totalHours);
+        
+               }if(selection == 1){
+                  JOptionPane.showMessageDialog(null,"Coming Soon");              
+               }
+               
+        }
 }
