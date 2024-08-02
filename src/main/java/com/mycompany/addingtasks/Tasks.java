26e2792f330963ef52e802b7cb85ebb0e5f6a1d8
ambercoltman <ST10438993@vcconnect.edu.za>
2024-08-02 14:46:02 +0200

    Resubmission edits
    Added arrays
    Added methods

--- src/main/java/com/mycompany/addingtasks/Tasks.java
+++ src/main/java/com/mycompany/addingtasks/Tasks.java
@@ -23,6 +23,9 @@ public class Tasks {
      public int totalHours;
      public String task;
      public String taskStatus;
+     public String developersDetails;
+     public String taskNameList;
+     public int taskDuration;
   
      /*Reference if...else statement
      Farrell(2023) states that the if...else statement does one actions as long as the boolean expression is true, and if it is false it performs a different action
@@ -36,22 +39,69 @@ public class Tasks {
       return false;
       }
     
-    public boolean task(String developerDetails, String taskName, String taskID, int totalHours, String taskStatus){
-        this.developerDetails = developerDetails;
-        this.taskName = taskName;
-        this.taskID = taskID;
-        this.totalHours = totalHours;
-        this.taskStatus = taskStatus;
-       return false;
-        
-      
-}
+  
     
+    public static String Options[][] = new String[6][1];
+    public static void optionChoices(){
+        Options[0][1]= "Done Tasks";
+        Options[1][1]= "Longest Task";
+        Options[2][1]= "Search by task";
+        Options[3][1]= "Search by developer";
+        Options[4][1]= "Display Report";
+        Options[5][1]= "Exit";
+    }
+    public static String List[][] = new String[4][4];
+    public static void arrayList(){
+      List[0][0]="Mike Smith";
+      List[0][1]="Create Login";
+      List[0][2]="5";
+      List[0][3]="To Do";
+      List[1][0]="Edward Harrison";
+      List[1][1]="Create Add Features";
+      List[1][2]="8";
+      List[1][3]="Doing";
+      List[2][0]="Samantha Paulson";
+      List[2][1]="Create Reports";
+      List[2][2]="2";
+      List[2][3]="Done";
+    }
     
-}  
+    public String doneTasks(){
+      JOptionPane.showMessageDialog(null, "Done Tasks:"+"\n"+ "Developer:"+ List[2][0]+
+              "\n"+"Task Name:"+ List[2][1]+"\n"+ "Task Duration:"+ List[2][3]);  
+       return null;  
+    }
 
+    
+    public String longestTasks(){
+      JOptionPane.showMessageDialog(null, "The longest task:"+"\n"+"Developer"+ List[1][0]+
+              "\n"+ "Task Name:"+ List[1][1]+"\n"+ "Task Duration:"+ List[1][2]);  
+       return null;
+    }
+    
+    public String searchTask(){
+        
+       return null;
+        
+    }
+    
+    public String searchDeveloper(){
+        
+       return null;
+        
+    }
+    
+    public String displayReport(){
+        JOptionPane.showMessageDialog(null, "Done Tasks:"+"\n"+ "Developer:"+ List[2][0]+
+                  "\n"+"Task Name:"+ List[2][1]+"\n"+ "Task Duration:"+ List[2][3]+"\n" +
+                  "The longest task:"+"\n"+"Developer"+ List[1][0]+"\n"+ "Task Name:"+ 
+                  List[1][1]+"\n"+ "Task Duration:"+ List[1][2]);
+       return null;
+        
+    }
+}
             /*Referencing 
             Farrel,J.2023.Java Programming Tenth Edition.Boston: Cengage
             */  
 
-    
+                          
